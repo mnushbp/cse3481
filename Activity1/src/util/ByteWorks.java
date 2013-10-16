27f32745fba59a84787d177e264302e0b57ab4e9
macarr <matthew.carr96@gmail.com>
2013-10-16 16:08:31 -0400

    updated byteworks with hex stuff

--- Activity1/src/util/ByteWorks.java
+++ Activity1/src/util/ByteWorks.java
@@ -53,5 +53,30 @@ public class ByteWorks
 	   }
 	   
    }
+   
+   public static String byteToHex(byte[] ar)
+	{
+		assert ar != null;
+		String result = "";
+		for (int i = 0; i < ar.length; i++)
+		{
+			int x = ar[i] & 0x000000FF;
+			String tmp = Integer.toHexString(x);
+			if (x < 16) tmp = "0" + tmp;
+			result += tmp;
+		}
+		return result.toUpperCase();
+	}
+	
+	public static byte[] hexToByte(String hex)
+	{
+		assert hex != null && hex.length() % 2 == 0;
+		byte[] result = new byte[hex.length() / 2];
+		for (int i = 0; i < hex.length(); i = i + 2)
+		{
+			result[i / 2] = (byte) Integer.parseInt(hex.substring(i, i + 2), 16);
+		}
+		return result;
+	}
 
 }
