f91a981b055c6e48d91925d29c41807ad01536f6
macarr <matthew.carr96@gmail.com>
2013-10-16 16:36:49 -0400

    side activities downloaded from course website

---
+++ cse3481_activities/src/util/ByteWorks.java
@@ -0,0 +1,82 @@
+package util;
+
+import java.io.*;
+ 
+/** General utility methods useful for working with byte arrays
+ * @author Matt Carr
+ * @version 0.1
+ */
+public class ByteWorks
+{
+
+   /** Empty to prevent instantiation */
+   private ByteWorks(){ 
+	   
+   }
+
+   /** 
+    * Read the given file into an array of bytes 
+    **/
+   public static byte[] fileToBytes(String filename) throws Exception {
+	   File file = new File(filename);
+	   int read;
+	   FileInputStream in;
+	   ByteArrayOutputStream out;
+	   in = new FileInputStream(file);
+	   out = new ByteArrayOutputStream();
+
+	   read = in.read();
+	   
+	   while(read != -1) {
+		   out.write(read);
+		   read = in.read();
+	   }
+	   in.close();
+	   return out.toByteArray();
+	   
+   }
+
+   /** 
+    * Create a file with the given name and write the given array of bytes to it 
+    **/ 
+   public static void bytesToFile(byte[] data, String filename) throws Exception {
+	   File file = new File(filename);
+	   FileOutputStream out = new FileOutputStream(file);
+	   try {
+		   if(!(file.exists()))
+			   file.createNewFile();
+		   if(!(file.canWrite()))
+			   throw new IOException("Cannot write to target file");
+		   out.write(data);
+	   } finally {
+		   out.close();
+	   }
+	   
+   }
+   
+   public static String byteToHex(byte[] ar)
+	{
+		assert ar != null;
+		String result = "";
+		for (int i = 0; i < ar.length; i++)
+		{
+			int x = ar[i] & 0x000000FF;
+			String tmp = Integer.toHexString(x);
+			if (x < 16) tmp = "0" + tmp;
+			result += tmp;
+		}
+		return result.toUpperCase();
+	}
+	
+	public static byte[] hexToByte(String hex)
+	{
+		assert hex != null && hex.length() % 2 == 0;
+		byte[] result = new byte[hex.length() / 2];
+		for (int i = 0; i < hex.length(); i = i + 2)
+		{
+			result[i / 2] = (byte) Integer.parseInt(hex.substring(i, i + 2), 16);
+		}
+		return result;
+	}
+
+}
