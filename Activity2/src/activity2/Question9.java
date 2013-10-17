27ac70fda4a3e1079ca127a38b9e5fccecd6caf9
macarr <matthew.carr96@gmail.com>
2013-10-17 15:06:22 -0400

    activity 2 uploaded

---
+++ Activity2/src/activity2/Question9.java
@@ -0,0 +1,19 @@
+package activity2;
+
+/*
+ *  This is a programming question:*
+ *
+ * Encrypt the plaintext "Facebook" using DES without any padding or 
+ * mode of operation using the key "universe" and output the ciphertext 
+ * in binary (use the byteToBin method in util). Next, generate a random 
+ * number between 0 and 63; flip that bit in the plaintext; encrypt; and 
+ * compute the number of bits in the ciphertext that have flipped as a 
+ * result. Re-do this avalanche effect computation several times; average 
+ * the results; and write down (in the txt file of this question) the 
+ * average number of flipped bits in the ciphertext.
+ *
+ */
+
+public class Question9 {
+
+}
