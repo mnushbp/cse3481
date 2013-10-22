eee511db5d2a8a9744579367bbde997a6dba6863
macarr <matthew.carr96@gmail.com>
2013-10-22 13:45:43 -0400

    butts

--- Activity2/src/activity2/Question10.java
+++ Activity2/src/activity2/Question10.java
@@ -1,5 +1,14 @@
 package activity2;
 
+import java.security.Key;
+import java.security.spec.AlgorithmParameterSpec;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+
+import util.ByteWorks;
+
 /*
  * This is a programming question:*
  *
@@ -20,9 +29,26 @@ package activity2;
 
 public class Question10 {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		
-		
+		// Encrypt "HelloCSE" with key "MyOwnKey"
+				Key secret = new SecretKeySpec(ByteWorks.hexToByte(
+						"9F0DCEDB322F3C6873F9256E01376BA4"), "AES");
+				AlgorithmParameterSpec aps = new IvParameterSpec(
+						ByteWorks.hexToByte("20FC19123087BF6CAC8D0F1254123004"));
+				Cipher ce = Cipher.getInstance("AES/CBC/PKCS5Padding");
+				byte[] ct = ByteWorks.hexToByte("F38ADBA8A7B4CC613578355032205D50");
+				System.out.println("CT = " + ByteWorks.byteToHex(ct));
+				ce.init(Cipher.DECRYPT_MODE, secret, aps);
+				byte[] back = ce.doFinal(ct);
+				String english = "";
+				for(byte b : back) {
+					
+					english += (char) b;
+					
+				}
+				System.out.println("PT = " + english);
+
 		
 	}
 
