121d97b946a2696a8e79292f68642eb41c18a6a9
macarr <matthew.carr96@gmail.com>
2013-12-01 12:05:35 -0500

    finished q10

--- Activity4/src/activity4/Q10.java
+++ Activity4/src/activity4/Q10.java
@@ -4,8 +4,31 @@ public class Q10 {
 	
 	public static void main(String[] args) {
 		
+		double total = 1;
+		double prob = 1;
 		
+		for(double i = 1; i < 30; i++) {
+			prob *= 1 - i/365;
+		}
+		
+		total = 1 - prob;
+		
+		System.out.println(total); //a
+		
+		total = 0;
+		prob = 1;
+		double j = 0;
+		
+		while (total < 0.99) {
+			prob *= 1 - ++j/365;
+			total = 1-prob;
+		}
+		System.out.println(j--); //b
+		System.out.println(total); //b
 		
 	}
 
 }
+//a) p = 0.706
+//b) 56 people
+//c) 365
